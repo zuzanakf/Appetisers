47129dc267deff8733e826827dc5728152306d1a
Zuzana Fernandes <zuzana.fernandes@yahoo.co.uk>
2025-05-26 14:05:40 +0100

    Initial Commit

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by Zuzana.Fernandes on 26/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppetizerTests/AppetizerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerTests.swift
+//  AppetizerTests
+//
+//  Created by Zuzana.Fernandes on 26/05/2025.
+//
+
+import Testing
+@testable import Appetizer
+
+struct AppetizerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
